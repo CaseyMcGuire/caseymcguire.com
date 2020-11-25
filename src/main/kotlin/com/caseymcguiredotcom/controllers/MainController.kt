be0e59d08c504805808f26c40cd121e9d7478fcc
Casey McGuire <caseyjaymcguire@gmail.com>
2020-11-24 20:25:17 -0800

    Remove extraneous injection from main controller.

--- src/main/kotlin/com/caseymcguiredotcom/controllers/MainController.kt
+++ src/main/kotlin/com/caseymcguiredotcom/controllers/MainController.kt
@@ -1,12 +1,10 @@
 package com.caseymcguiredotcom.controllers
 
-import generated.jooq.tables.references.POSTS
-import org.jooq.DSLContext
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class MainController(val context: DSLContext) {
+class MainController {
 
   @GetMapping(value = [
     "/",
@@ -15,8 +13,6 @@ class MainController(val context: DSLContext) {
     "/posts/page/{id}"
   ])
   fun home(): String {
-    val result = context.select().from(POSTS).fetch()
-    println(result)
     return "index"
   }
 }
