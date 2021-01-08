30bd605db4128fcc316a650a7b14cc4cd3e80187
Casey McGuire <caseyjaymcguire@gmail.com>
2021-01-07 21:19:21 -0800

    Add a 1 second delay to the loading glimmer.

--- src/main/web-frontend/components/LoadingShimmer/LoadingShimmer.tsx
+++ src/main/web-frontend/components/LoadingShimmer/LoadingShimmer.tsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 import {createUseStyles} from "react-jss";
+import {useEffect, useState} from "react";
 
 const useStyles = createUseStyles({
   '@keyframes shimmer': {
     from: {
-      backgroundColor: '#dbdbdb'
+      opacity: 1
     },
     to: {
-      backgroundColor: '#f3f3f3'
+      opacity: 0.25
     }
   },
   shimmerContainer: {
+    backgroundColor: '#e3e3e3',
     animationName: '$shimmer',
     animationDuration: '1s',
     animationDirection: 'alternate',
@@ -24,7 +26,20 @@ export default function LoadingShimmer(props: {
   width: number | string,
   marginBottom?: number
 }) {
+  const [isShown, setIsShown] = useState(false);
+  useEffect(() => {
+    const timer = window.setTimeout(
+      () => setIsShown(() => true),
+      1000
+    );
+    return () => {
+      window.clearTimeout(timer);
+    }
+  }, [isShown])
   const styles = useStyles();
+  if (!isShown) {
+    return null;
+  }
   return (
     <div style={{...props}} className={styles.shimmerContainer}/>
   );
