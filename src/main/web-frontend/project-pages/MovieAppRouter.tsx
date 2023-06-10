1e0fadcedc17cc56c6080f2ec133f1298e7eebe9
CaseyMcGuire <caseyjaymcguire@gmail.com>
2023-06-10 14:31:49 -0700

    Got basic sidebar working for movie app.

--- src/main/web-frontend/project-pages/MovieAppRouter.tsx
+++ src/main/web-frontend/project-pages/MovieAppRouter.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {graphql} from "react-relay";
 import {useLazyLoadQuery} from "react-relay/hooks";
 import {MovieAppRouterQuery} from "../__generated__/MovieAppRouterQuery.graphql";
+import MovieRoot from "./MovieRoot";
+import {BrowserRouter, Route, Router, Switch} from "react-router-dom";
 
 export default function MovieAppRouter() {
   const query = graphql`
@@ -16,8 +18,22 @@ export default function MovieAppRouter() {
 
   const response = useLazyLoadQuery<MovieAppRouterQuery>(query, {})
   return (
-    <div>
-      {response?.movieApi?.movie?.title}
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route key={"alsjdf"}
+               exact
+               path={"/movies"}
+               render={(props) => {
+                 return <MovieRoot>
+                   <div>
+                     {response?.movieApi?.movie?.title}
+                   </div>
+                 </MovieRoot>
+               }}
+
+               />
+      </Switch>
+    </BrowserRouter>
+
   )
 }
---
+++ src/main/web-frontend/project-pages/MovieSidebar.tsx
@@ -0,0 +1,55 @@
+import {createUseStyles} from "react-jss";
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+const useStyles = createUseStyles({
+  sidebar: {
+    minWidth: '96px',
+    minHeight: '90%',
+    maxHeight: '960px',
+    position: 'fixed',
+    backgroundColor: '#404040',
+    margin: '20px',
+    borderRadius: '8px'
+  },
+  sidebarContents: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    marginTop: '36px',
+    marginBottom: '76px'
+  },
+  navIcon: {
+    height: '24px',
+    width: '24px',
+    margin: '20px',
+    '&:hover': {
+      // changing the color of SVGs defined in img tags is not straightforward. Thus, I used this hacky workaround
+      // I found here: https://css-tricks.com/change-color-of-svg-on-hover/#aa-svg-background-images
+      // using this tool here: https://codepen.io/sosuke/pen/Pjoqqp
+      // note that that the first brightness and saturate attributes turn the SVG to black first otherwise the rest of
+      // the attributes won't work (see description in link above)
+      filter: 'brightness(0) saturate(100%) invert(34%) sepia(81%) saturate(2400%) hue-rotate(338deg) brightness(113%) contrast(100%)',
+    }
+  }
+})
+
+export default function MovieSidebar() {
+  const styles = useStyles();
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.sidebarContents}>
+        <img className={styles.logo} src={"/assets/images/movies/logo.svg"} />
+      <Link to={"/movies"}>
+        <img className={styles.navIcon} src={"/assets/images/movies/icon-nav-home.svg"} />
+      </Link>
+        <img className={styles.navIcon} src={"/assets/images/movies/icon-nav-movies.svg"} />
+        <img className={styles.navIcon} src={"/assets/images/movies/icon-nav-tv-series.svg"} />
+        <img className={styles.navIcon} src={"/assets/images/movies/icon-nav-bookmark.svg"} />
+      </div>
+    </div>
+  )
+}
