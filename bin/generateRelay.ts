4dade8e66a0a87e23e13c2f30239ffad21a21f82
CaseyMcGuire <caseyjaymcguire@gmail.com>
2024-09-23 21:04:02 -0700

    Add ability to compile Relay queries from multiple GraphQL files.

---
+++ bin/generateRelay.ts
@@ -0,0 +1,10 @@
+import getGraphqlSchema from "./generateCombinedGraphQLSchema";
+import fs from "fs";
+import { execSync } from 'child_process'
+
+const existingSchemaFile = 'src/main/resources/relay/schema.graphql'
+
+const combinedSchema = getGraphqlSchema()
+fs.writeFileSync(existingSchemaFile, combinedSchema)
+execSync("relay-compiler")
+fs.writeFileSync(existingSchemaFile, "\"\"\"\n" + combinedSchema + "\n\"\"\"")
--- src/main/web-frontend/projects/WorkoutTracker/WorkoutTrackerRoot.tsx
+++ src/main/web-frontend/projects/WorkoutTracker/WorkoutTrackerRoot.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {graphql} from "react-relay";
 
 export function WorkoutTrackerRoot() {
+  const query = graphql`
+    query WorkoutTrackerRootQuery {
+      workoutTracker {
+        workouts {
+          id
+          description
+        }
+      }
+    }
+  `
   return <div>
     Workout Tracker!
   </div>
