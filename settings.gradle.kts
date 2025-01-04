0e1abd9e9a76ac25b82d5668fc4fe1fc060a205e
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-01-03 20:35:02 -0800

    Move gradle submodules to their own folder.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,19 @@
 rootProject.name = "caseymcguiredotcom"
 include("customgenerator")
+
+fun includeSubmodulesFromDirectory(directoryName: String) {
+  val submodulesDir = file(directoryName)
+  if (submodulesDir.exists() && submodulesDir.isDirectory) {
+    submodulesDir.listFiles()?.forEach { dir ->
+      if (dir.isDirectory && File(dir, "build.gradle.kts").exists()) {
+        val moduleName = dir.name
+        include(moduleName)
+        project(":$moduleName").projectDir = dir
+      }
+    }
+  } else {
+    println("Directory '$directoryName' does not exist or is not a directory.")
+  }
+}
+
+includeSubmodulesFromDirectory("submodules")
