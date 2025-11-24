6fbf8596932f2e23dc6c270ebbf7bb1f8095d71f
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-11-23 18:07:33 -0800

    Delete unused files.

--- src/main/kotlin/com/caseymcguiredotcom/views/PageUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.caseymcguiredotcom.views
-
-import kotlinx.html.HEAD
-import kotlinx.html.script
-
-fun HEAD.reactScripts() {
-  script(type = "text/javascript", src = "/assets/javascripts/react.production.min.js") {}
-  script(type = "text/javascript", src = "/assets/javascripts/react-dom.production.min.js") {}
-}
