99b3d32a01d30ca2bccedf9fad26797064150806
CaseyMcGuire <caseyjaymcguire@gmail.com>
2025-11-29 12:10:27 -0800

    Rename MainController to GraphqlController since it only contains one endpoint now.

--- src/main/kotlin/com/caseymcguiredotcom/controllers/GraphqlController.kt
+++ src/main/kotlin/com/caseymcguiredotcom/controllers/GraphqlController.kt
@@ -1,14 +1,12 @@
 package com.caseymcguiredotcom.controllers
 
-import com.caseymcguiredotcom.views.BlogPage
 import com.caseymcguiredotcom.views.ReactPage
-import kotlinx.html.*
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
-class MainController {
+class GraphqlController {
 
   @GetMapping("/graphiql")
   @ResponseBody
